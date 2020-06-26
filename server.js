7f8c6a5a843323a04c066650bef01e723dcda948
Harsh Tuwar <tuwarharsh08@gmail.com>
2020-06-25 21:38:19 -0300

    updates

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ require('./config/passport')(passport);
 // routes
 app.use('/api/users', users);
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`We're live in '${process.env.NODE_ENV}' mode on port ${PORT}`.blue.bold));
